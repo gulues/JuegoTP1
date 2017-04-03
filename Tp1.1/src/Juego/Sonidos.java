e7562704a6fe1b3dac9556246b4b24d981be21ff
gestevez <gestevez@CAECOPAZ02.caecopaz.mil.ar>
2017-04-03 13:52:57 -0300

    Fix Format

--- Tp1.1/src/Juego/Sonidos.java
+++ Tp1.1/src/Juego/Sonidos.java
@@ -15,70 +15,40 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Sonidos implements LineListener {
 
-	/**
-	 * this flag indicates whether the playback completes or not.
-	 */
 	boolean playCompleted;
 
-	/**
-	 * Play a given audio file.
-	 * 
-	 * @param audioFilePath
-	 *            Path of the audio file.
-	 */
 	void play(String audioFilePath) {
 		File audioFile = new File(audioFilePath);
 
 		try {
 			AudioInputStream audioStream = AudioSystem
 					.getAudioInputStream(audioFile);
-
 			AudioFormat format = audioStream.getFormat();
-
 			DataLine.Info info = new DataLine.Info(Clip.class, format);
-
 			Clip audioClip = (Clip) AudioSystem.getLine(info);
-
 			audioClip.addLineListener(this);
-
 			audioClip.open(audioStream);
-
 			audioClip.start();
+			
 			if (!playCompleted)
-				;
-			// while (!playCompleted) {
-			// int b;//Thread.sleep(10);
-			// }
-
 			audioClip.close();
 
 		} catch (UnsupportedAudioFileException ex) {
-			System.out.println("The specified audio file is not supported.");
-			ex.printStackTrace();
-		} catch (LineUnavailableException ex) {
-			System.out.println("Audio line for playing back is unavailable.");
-			ex.printStackTrace();
-		} catch (IOException ex) {
-			System.out.println("Error playing the audio file.");
 			ex.printStackTrace();
 		}
+		catch (LineUnavailableException ex){ 
+			ex.printStackTrace();
+		}
+		 catch (IOException ex){ 
+			ex.printStackTrace();
+		 }
+		
 
 	}
 
-	/**
-	 * Listens to the START and STOP events of the audio line.
-	 */
 	@Override
-	public void update(LineEvent event) {
-		// LineEvent.Type type = event.getType();
-		//
-		// if (type == LineEvent.Type.START) {
-		// System.out.println("Playback started.");
-		//
-		// } else if (type == LineEvent.Type.STOP) {
-		// playCompleted = true;
-		// System.out.println("Playback completed.");
-		// }
-
+	public void update(LineEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
