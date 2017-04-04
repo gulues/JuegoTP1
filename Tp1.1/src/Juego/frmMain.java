e847eafa2d0e06650598bc257766d2d79eaec5fc
gestevez <gestevez@CAECOPAZ02.caecopaz.mil.ar>
2017-04-04 12:59:54 -0300

    Dispose???

--- Tp1.1/src/Juego/frmMain.java
+++ Tp1.1/src/Juego/frmMain.java
@@ -12,15 +12,14 @@ import javax.swing.JOptionPane;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-
 public class frmMain {
-	private int[][] Matriz ;
-	private int movimientos=1;
+	private int[][] Matriz;
+	private int movimientos = 1;
 	private JLabel lblMovimientos;
 	private JLabel[] cuadro = new JLabel[16];
 	public JFrame frmRompeCabezas;
-	private Tablero tbl = new Tablero(4);
-	
+	private Tablero tbl;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -40,7 +39,7 @@ public class frmMain {
 	}
 
 	private void initialize() {
-		
+
 		frmRompeCabezas = new JFrame();
 		frmRompeCabezas.setBounds(100, 100, 491, 544);
 		frmRompeCabezas.setLocationRelativeTo(null);
@@ -48,13 +47,12 @@ public class frmMain {
 		frmRompeCabezas.setResizable(false);
 		frmRompeCabezas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmRompeCabezas.getContentPane().setLayout(null);
-
-		
+		 
+		tbl = new Tablero(4);
 		crearCuadros();
 
-
 		JLabel lblFondo = new JLabel("");
-		lblFondo.setIcon(new ImageIcon( "img/cuadrado.png"));
+		lblFondo.setIcon(new ImageIcon("img/cuadrado.png"));
 		lblFondo.setBounds(0, 50, 484, 465);
 		frmRompeCabezas.getContentPane().add(lblFondo);
 
@@ -65,25 +63,28 @@ public class frmMain {
 		JLabel lblTiempo = new JLabel("Tiempo:");
 		lblTiempo.setBounds(135, 11, 46, 14);
 		frmRompeCabezas.getContentPane().add(lblTiempo);
-		
+
 		JButton btnReiniciar = new JButton("Reiniciar");
 		btnReiniciar.setBounds(228, 7, 89, 23);
 		frmRompeCabezas.getContentPane().add(btnReiniciar);
 		JButton btnAyuda = new JButton("Ayuda");
 		btnAyuda.setBounds(330, 7, 89, 23);
 		frmRompeCabezas.getContentPane().add(btnAyuda);
-			
-		
-		
-//Accciones
+
+		// Accciones
 		btnReiniciar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				 int restart=JOptionPane.showConfirmDialog (null, "¿Desea reiniciar la partida?","Warning",JOptionPane.YES_NO_OPTION);
-				if (restart==0) {
-					crearCuadros();
+				int restart = JOptionPane.showConfirmDialog(null,
+						"¿Desea reiniciar la partida?", "Warning",
+						JOptionPane.YES_NO_OPTION);
+				if (restart == 0) {
+					//HACER
+					
+					frmRompeCabezas.dispose();
+					frmMain.main(null);
+					
 				}
-				 //frmWin.main(null);
 			}
 		});
 	}
@@ -92,59 +93,59 @@ public class frmMain {
 		int cont = 0;
 		int posX = 0;
 		int posY = 0;
-		
+
 		Matriz = tbl.getTabla();
-		
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++) 
-			{
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
 				cuadro[cont] = new JLabel();
 				cuadro[cont].setText(i + "," + j);
-				cuadro[cont].setIcon(new ImageIcon("img/"+ Matriz[i][j] + ".png"));
+				cuadro[cont].setIcon(new ImageIcon("img/" + Matriz[i][j]+ ".png"));
 				cuadro[cont].setBounds(50 + posX, 105 + posY, 95, 95);
-				
 				posX = posX + 100;
-				setActionListened(cont++);
-				frmRompeCabezas.getContentPane().add(cuadro[cont - 1]);
+				setActionListened(cont);
+				frmRompeCabezas.getContentPane().add(cuadro[cont++]);
 			}
 			posY = posY + 95;
 			posX = 0;
 		}
 	}
-//Crear las acciones del clic en cada uno de los Cuadros: Jlabel[]
+	
+	
+	
+	// Crear las acciones del clic en cada uno de los Cuadros: Jlabel[]
 	private void setActionListened(int index) {
 		cuadro[index].addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
 				JLabel labelAux = (JLabel) e.getSource();
 				String str = labelAux.getText();
 				String[] coord = str.split(",");
-				Point cuadroXY= new Point(Integer.parseInt(coord[0]),Integer.parseInt(coord[1]));
-				
-				if(tbl.moverCuadrado(cuadroXY)){
+				Point cuadroXY = new Point(Integer.parseInt(coord[0]), Integer
+						.parseInt(coord[1]));
+
+				if (tbl.moverCuadrado(cuadroXY)) {
 					Matriz = tbl.getTabla();
-					int cont=0;
+					int cont = 0;
 					lblMovimientos.setText("Movimientos: " + movimientos++);
-					
+
 					for (int i = 0; i < tbl.size; i++) {
 						for (int j = 0; j < tbl.size; j++) {
 							cuadro[cont].setText(i + "," + j);
-							cuadro[cont++].setIcon(new ImageIcon( "img/"+ Matriz[i][j] + ".png"));
+							cuadro[cont++].setIcon(new ImageIcon("img/"+ Matriz[i][j] + ".png"));
 						}
 					}
-				if (tbl.checkWin()){
-					playSound("win");
-		
+					if (tbl.checkWin()) {
+						playSound("win");
+
 					}
 				}
 			}
 		});
-		
+
 	}
 
-	private static void playSound(String sound){
-		String audioFilePath =  "sounds/"+sound+".wav";
-        Sonidos player = new Sonidos();
-        player.play(audioFilePath);
+	private static void playSound(String sound) {
+		String audioFilePath = "sounds/" + sound + ".wav";
+		Sonidos player = new Sonidos();
+		player.play(audioFilePath);
 	}
 }
